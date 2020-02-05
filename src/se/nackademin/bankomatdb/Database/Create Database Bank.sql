db089611ffb38b7e5fcf64d2b077c497e06eafed
Jaanek Kerma <Jaanek.Kerma@yh.nackademin.se>
2020-02-05 13:44:41 +0100

    added more data to db (5 test konto) created trigger after_konto_update which inserts into transaktion necessary info when saldo in konto is updated changed db. (removed unnecessay tables(äger, tar_ut, historik)) Necessery data moved

--- src/se/nackademin/bankomatdb/Database/Create Database Bank.sql
+++ src/se/nackademin/bankomatdb/Database/Create Database Bank.sql
@@ -14,28 +14,18 @@ create table if not exists Konto(
 	Kontonummer int not null auto_increment,
     Saldo int not null,
     Räntesats int not null,
-    primary key(Kontonummer)
-);
-
-create table if not exists Äger(
 	Kund int not null,
-    Konto int not null,
-    foreign key(Kund) references Kund(Kundnummer),
-    foreign key(Konto) references Konto(Kontonummer)
+    primary key(Kontonummer),
+    foreign key(Kund) references Kund(Kundnummer)
 );
 
 create table if not exists Transaktion(
 	TransaktionID int not null auto_increment,
     Tidpunkt datetime not null,
     Saldoförändring int not null,
-    primary key(TransaktionID)
-);
-
-create table if not exists Historik(
-	Konto int not null,
-    Transaktion int not null,
-    foreign key(Konto) references Konto(Kontonummer),
-    foreign key(Transaktion) references Transaktion(TransaktionID)
+    Konto int not null,
+    primary key(TransaktionID),
+    foreign key(Konto) references Konto(Kontonummer)
 );
 
 create table if not exists Lån(
@@ -44,12 +34,7 @@ create table if not exists Lån(
     Räntesats int not null,
     Betalplan int not null,
     Beviljades datetime not null,
-    primary key(ID)
-);
-	
-create table if not exists Tar_ut(
-	Kund int not null,
-    Lån int not null,
-    foreign key(Kund) references Kund(Kundnummer),
-    foreign key(Lån) references Lån(ID)
+    Kund int not null,
+    primary key(ID),
+    foreign key(Kund) references Kund(Kundnummer)
 );
