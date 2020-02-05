939ba351bc5dfc6100ed5ab89e9b4b7651e0a3c9
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2020-02-05 11:55:14 +0100

    Tydligare datamodeller.

--- src/se/nackademin/bankomatdb/model/DTOCustomer.java
+++ src/se/nackademin/bankomatdb/model/DTOCustomer.java
@@ -2,11 +2,19 @@ package se.nackademin.bankomatdb.model;
 
 // TODO
 public final class DTOCustomer {
+    private final int id;
+    private final String name;
+
+    public DTOCustomer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getCustomerId() {
-        return 0;
+        return id;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 }
