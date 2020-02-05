2099e7de2dad932784fce3db3ea5aeb6f14d75ee
Jaanek Kerma <Jaanek.Kerma@yh.nackademin.se>
2020-02-05 12:53:11 +0100

    testing connection to Google Cloud SQL

---
+++ src/se/nackademin/bankomatdb/Database/Test.java
@@ -0,0 +1,33 @@
+package se.nackademin.bankomatdb.Database;
+
+import se.nackademin.bankomatdb.adminpanel.View.LåneHanteringView;
+
+import java.sql.*;
+
+
+public class Test {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        try (Connection con = DriverManager.getConnection(
+                "jdbc:mysql://35.228.210.231:3306/Bank",
+                "root",
+                "159357");
+             Statement stmt = con.createStatement()
+        ) {
+            ResultSet rs;
+
+            rs = stmt.executeQuery("select * from Kund");
+            StringBuilder sb = new StringBuilder();
+            System.out.println("Kundnummer | Namn | Persunnummer | Pin");
+            while (rs.next()) {
+                sb.append(rs.getInt("Kundnummer")).append(" | ");
+                sb.append(rs.getString("Namn")).append(" | ");
+                sb.append(rs.getInt("Personnummer")).append(" | ");
+                sb.append(rs.getInt("Pin")).append("\n");
+            }
+            System.out.println(sb);
+
+        }
+    }
+}
